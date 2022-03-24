257f88a07de017de35c17694d9337e77e053ca89
ungalexy <ungalexy@gmail.com>
2022-03-24 19:25:26 +0200

    prep debank tests with mocha chai

--- tests/DeBank.tests.js
+++ tests/DeBank.tests.js
@@ -2,6 +2,6 @@ const TetherMock = artifacts.require('TetherMock');
 const RewardToken = artifacts.require('RewardToken');
 const DeBank = artifacts.require('DeBank');
 
-require('chai')
-    .use()
-    .should()
+// require('chai')
+//     .use()
+//     .should()   
