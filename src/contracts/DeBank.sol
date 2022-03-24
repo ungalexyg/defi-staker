aa11ec4d3714d2515f0909b99e5441480460193a
ungalexy <ungalexy@gmail.com>
2022-03-24 11:49:19 +0200

    add debbank contract

--- src/contracts/DeBank.sol
+++ src/contracts/DeBank.sol
@@ -1,9 +1,21 @@
 pragma solidity ^0.5.0;
 
+// imports can be used as in js, the .sol is required
+import "./RewardToken.sol";
+import "./TetherMock.sol";
+
 /**
  * DeBank contract
  */
 contract DeBank {
     string public name = "DeBank";
     address public owner;
+
+    TetherMock public stablecoin;
+    RewardToken public rwrd;
+
+    constructor(RewardToken _rwrd, TetherMock _tether) public {
+        rwrd = _rwrd;
+        stablecoin = _tether;
+    }
 }
