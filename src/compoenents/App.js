00b1768fee541947a80be52210052a0c642bd1c8
ungalexy <ungalexy@gmail.com>
2022-03-24 20:34:58 +0200

    prep web3 loader

---
+++ src/compoenents/App.js
@@ -0,0 +1,17 @@
+
+import Web3 from 'web3';
+
+function App() {
+    /*
+    async Web3() {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+        } else {
+            if (window.web3) {
+                window.web3 = new Web3(window.web3.currentProvider);
+            } else {
+                window.alert("No ethereum browser detected. You can check out MetaMask!");
+            }
+        }
+    }*/
+}
