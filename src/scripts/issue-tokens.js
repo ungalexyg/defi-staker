758ddc181b43e26f650c72defb3754dc1f150f4b
ungalexy <ungalexy@gmail.com>
2022-03-24 20:34:42 +0200

    minor update

--- src/scripts/issue-tokens.js
+++ src/scripts/issue-tokens.js
@@ -1,8 +1,8 @@
-const deBank = artifacts.require('DeBank');
+const DeBank = artifacts.require('DeBank');
 
 
 module.exports = async function issueReward(callback) {
-    let deBank = await deBank.deployed();
+    let deBank = await DeBank.deployed();
     await deBank.issueTokens();
     console.log("Tokens have been issues successfully");
     callback();
